27f5b0c1866523d37e9b634cb8be83e373b85c0c
Jakub Jakubowski <kubajot6@wp.pl>
2022-10-30 19:25:10 +0100

    Add animation to categories buttons

--- src/components/header/CategoriesExpand.js
+++ src/components/header/CategoriesExpand.js
@@ -1,14 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { counterActions, visibleActions } from '../../store/store';
 import classes from './CategoriesExpand.module.css';
 
 const CategoriesExpand =(props)=>{
     const dataFetch = useSelector(state => state.products.data);
+    const [changeCategory, setChangeCategory] = useState(false)
 
     useEffect(()=>{
         console.log(dataFetch);
     },[dataFetch])
+
+    useEffect(()=>{
+        setChangeCategory(true);
+        const timer = setTimeout(() => {
+            setChangeCategory(false);
+          }, 200);
+          return () => clearTimeout(timer);;
+    },[props.category])
 
     const createExpandList =(category)=> {
         const product = dataFetch.filter((item)=> 
@@ -30,13 +39,16 @@ const CategoriesExpand =(props)=>{
         item.category === mainProduct.category && item.title !== mainProduct.title); 
 
         return (
-            <ul>
+            <ul className={classes.productsList}>
+                <div className={classes.currentCategoryProducts}>
                 <h4>{mainProduct.title}</h4>
                 <br/>
-                {currentCategoryItems.map((product)=><li key={product.id} className={classes.items}>{product.title}</li>)}
+                 {currentCategoryItems.map((product)=><li key={product.id} className={classes.items}>{product.title}</li>)}   
+                </div>
+                
 
                 { otherCategoryNames.map((category)=> 
-                <div key={category}> 
+                <div className={classes.otherCategoriesProducts} key={category}> 
                     <br/> 
                     <h4>{category}</h4>
                     {dataFetch.filter((item)=>item.category === category)
@@ -50,7 +62,7 @@ const CategoriesExpand =(props)=>{
     }
 
     return(
-        <div onMouseEnter={()=>props.onMouseHandler(true)} onMouseLeave={()=>props.onMouseHandler(false)} className={classes.categoriesExpand}>
+        <div onMouseEnter={()=>props.onMouseHandler(true)} onMouseLeave={()=>props.onMouseHandler(false)} className={ !changeCategory ? classes.categoriesExpand : `${classes.categoriesExpand} ${classes.flashAnimation}`}>
             { props.category && dataFetch && createExpandList(props.category)}
         </div>
     )
