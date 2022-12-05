233fbe5b4050a975e67cfcef142379316651df55
kubajot90 <kubajot90@gmail.com>
2022-12-05 20:43:08 +0100

    Add Banner-1

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,8 @@ import { useDispatch,  useSelector } from 'react-redux';
 import { useEffect, useRef } from 'react';
 import HeroVideoBaner from './HeroVideoBaner';
 import ProductsSlider from './ProductsSlider';
-import fetchProductsData from '../../store/productsSlice'
+import fetchProductsData from '../../store/productsSlice';
+import Baner_1 from './baners/Baner_1';
 
 
  const Main =()=> {
@@ -19,6 +20,7 @@ import fetchProductsData from '../../store/productsSlice'
         <>
             <HeroVideoBaner/>
             {fetchProducts && <ProductsSlider category={"women's clothing"} observer={true}/>}
+            <Baner_1/>
             {fetchProducts && <ProductsSlider category={"men's clothing"} observer={true}/>}
             {fetchProducts && <ProductsSlider category={"jewelery"} observer={true}/>}
         </>
